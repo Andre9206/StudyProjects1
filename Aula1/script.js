45da2ead5ef051c1546c497bb50333cd7e2b4dcc
Andre Munhoz <51175759+Andre9206@users.noreply.github.com>
2019-06-07 16:59:18 -0300

    Update script.js

--- Aula1/script.js
+++ Aula1/script.js
@@ -2,8 +2,8 @@
   var msg = window.document.getElementById('msg');
   var img = window.document.getElementById('imagem');
   var data = new Date();
-  // var hora = data.getHours();
-  var hora = 22                                 //PARA FORÇAR A HORA AO REALIZAR TESTES.
+  var hora = data.getHours();
+  //var hora = 22                                 //PARA FORÇAR A HORA AO REALIZAR TESTES.
   msg.innerHTML = `Agora são: ${hora} horas.`;
   if (hora >= 0 && hora < 12) {
     // BOM DIA
